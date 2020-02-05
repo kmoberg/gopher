c90618500c240cbbb2a3f4a3fee806f448e995bb
Karl Mathias Moberg <karlmathias@gmail.com>
2020-02-05 20:27:50 +0100

    Initial commit

--- main.go
+++ main.go
@@ -11,16 +11,19 @@ import (
 
 func main() {
 
+	// Generate the submission in a pretty JSON format
 	requestBody, err := json.Marshal(map[string]string{
 		"name" : "Karl Mathias Moberg",
 		"email" : "karl.moberg@evry.com",
 		"source_code" : "https://github.com/kmoberg/gopher",
 	})
 
+	// Do error handling, just in case.
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Post
 	resp, err := http.Post("http://10.200.233.89:8080/contest", "application/json", bytes.NewBuffer(requestBody))
 	if err != nil {
 		log.Fatalln(err)
@@ -28,11 +31,15 @@ func main() {
 
 	defer resp.Body.Close()
 
+	// Get response
 	body, err := ioutil.ReadAll(resp.Body)
+
+	// More error handling. 
 	if err != nil {
 		log.Fatalln(err)
 	}
 
+	// Print the response
 	log.Println(string(body))
 
 
